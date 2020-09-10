d49ee6d3431007452acf3eae4fb03de580e7e935
tomaszmyhal <tomasz.myhal@gmail.com>
2020-09-10 12:53:19 +0000

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('AAAAAAAAAA');
+const express = require('express');
+const fileRouter = require('./routes/file');
+const app = express();
+
+app.use('/api/v1', fileRouter);
+
+app.listen(3005, () => console.log('API started'));
